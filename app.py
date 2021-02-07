65d2f0eaecce879513b1599d3a097a4b6013e430
ullyzian <aviev2000@icloud.com>
2021-02-07 15:45:21 +0100

    Connect postgres to instances

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+import psycopg2
 import socket
+
+conn = psycopg2.connect(dbname="database", user="user", password="password", host="10.106.41.56")
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return f'Hello World!\nIP: {socket.gethostbyname(socket.gethostname())}'
+    with conn.cursor() as curs:
+        curs.execute("SELECT version();")
+        version = curs.fetchone()
+    conn.close()
+    return f'Hello World with Kubernetes example!' \
+           f'\nIP:{socket.gethostbyname(socket.gethostname())}' \
+           f'\nPostgres version: {version}'
 
 
 if __name__ == '__main__':
